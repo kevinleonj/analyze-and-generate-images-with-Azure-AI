de1d8ef7e15240dda85d2ec98211637915b56a7c
Kevin Leon <kevinleonjouvin@hotmail.com>
2024-05-11 22:42:20 +0200

    First commit / local host working / no deployment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
-import React from 'react';
+// Import the React library to use in this file
+import React, { useState } from 'react';
 
+// Define a function component named App
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  // Declare a state variable named 'imageUrl' with its setter function 'setImageUrl'
+  // This will be used to store the URL of the image entered by the user
+  const [imageUrl, setImageUrl] = useState('');
+
+  // Define a function to handle the click event of the Analyze button
+  const handleAnalyzeClick = () => {
+    // For now, this function does nothing
+    // In the future, this function will call the Azure Computer Vision API to analyze the image
+  };
+
+  // Define a function to handle the click event of the Generate button
+  const handleGenerateClick = () => {
+    // For now, this function does nothing
+    // In the future, this function will call the OpenAI API to generate a text based on the image description
+  };
+
+  // Return the JSX to render
+  return (
+    <div>
+      {/* This is the title of the application */}
+      <h1>Azure AI Image Analyzer and Text Generator</h1>
+
+      {/* This is a form for the user to enter the URL of the image */}
+      <form>
+        <label>
+          {/* This is the label for the text box */}
+          Image URL:
+          {/* This is the text box for the user to enter the URL of the image */}
+          {/* The value of the text box is bound to the 'imageUrl' state variable */}
+          {/* When the user types in the text box, the 'setImageUrl' function is called to update the 'imageUrl' state variable */}
+          <input type="text" value={imageUrl} onChange={e => setImageUrl(e.target.value)} />
+        </label>
+      </form>
+
+      {/* This is the Analyze button */}
+      {/* When the button is clicked, the 'handleAnalyzeClick' function is called */}
+      <button onClick={handleAnalyzeClick}>Analyze Image</button>
+
+      {/* This is the Generate button */}
+      {/* When the button is clicked, the 'handleGenerateClick' function is called */}
+      <button onClick={handleGenerateClick}>Generate Text</button>
+    </div>
+  );
 }
 
+// Export the App component to be used in other files
 export default App;
